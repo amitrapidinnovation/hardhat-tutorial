07db3c3520215296ea6fef451f91d92db583b56a
Amit Mehta <amitmehta@Amits-MacBook-Air.local>
2022-11-10 11:43:32 +0530

    created-token-transfer-transaction-with-test-cases

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "Hardhat Token";
+    string public symbol = "HHT";
+    uint public totalSupply = 1000;
+
+    address public owner;
+
+    mapping(address => uint) balances;
+
+    constructor(){
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender; 
+    } 
+
+    function transfer(address to, uint amount) external {
+        console.log("**Sender balance is %s tokens**", balances[msg.sender]);
+        console.log("**Sender is sending %s tokens to %s address**", amount, to);
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount; // balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] += amount;
+    } 
+
+    function balanceOf(address account) external view returns(uint) {
+        return balances[account];
+    }
+}
